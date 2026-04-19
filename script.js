1e8ee48a7233031ed2e0d98d2e3c5272e4dc1c98
Caio Pedroso <caio.pedroso@getjones.com>
2026-04-19 18:28:26 -0300

    script groundwork

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function getGameboard() {
-  const gamestate = [[],[],[]]
-  console.log(gamestate)
-}
+const getGameboard = (() => {
+  const currentBoard = []
+  return currentBoard
+})();
 
-getGameboard()
+const createGameboard = (() => {
+  const newBoard = []
+  for (i = 0; i<9; i++) {newBoard.push(null)}
+  return newBoard
+})();
+
+const winConditions = (() => {
+  const combinations = [
+    [1, 2, 3],
+    [1, 4, 7],
+    [1, 5, 9],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+  return combinations
+})();
+
+const getPlayerOne = (() => {
+  const currentPicks = []
+  return currentPicks
+})();
+
+const getPlayerTwo = (() => {
+  const currentPicks = []
+  return currentPicks
+})();

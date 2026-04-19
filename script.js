dbc9c2fd59f6193d33be856a2cd7269f988e6232
Caio Pedroso <caio.pedroso@getjones.com>
2026-04-19 19:06:01 -0300

    object, iife and method

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-const getGameboard = (() => {
-  const currentBoard = []
-  return currentBoard
-})();
-
-const createGameboard = (() => {
-  const newBoard = []
-  for (i = 0; i<9; i++) {newBoard.push(null)}
-  return newBoard
-})();
+const Gameboard = {
+  currentBoard: (() => {
+    const newBoard = []
+    for (let i = 0; i < 9; i++) {newBoard.push(null)}
+    return newBoard
+  })(),
+  getBoard: () => Gameboard.currentBoard
+}
 
 const winConditions = (() => {
-  const combinations = [
-    [1, 2, 3],
+  const combinations = [ 
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
     [1, 4, 7],
-    [1, 5, 9],
     [2, 5, 8],
-    [3, 6, 9],
-    [3, 5, 7],
-    [4, 5, 6],
-    [7, 8, 9]
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
   ]
   return combinations
-})();
+});
 
-const getPlayerOne = (() => {
+const PlayerOne = (() => {
   const currentPicks = []
   return currentPicks
-})();
+});
 
-const getPlayerTwo = (() => {
+const PlayerTwo = (() => {
   const currentPicks = []
   return currentPicks
-})();
+});
+
+const makeMove = ((spot, player) => {
+  let newSpot = spot
+  return player
+});
+
+const gameDirector = (() => {
+  return null
+});

bb7aff15d10ffa111747b5e4808384ade2b12448
Caio Pedroso <caiovazpedroso@outlook.com>
2026-04-20 21:13:43 -0300

    to iife`s and factories

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const Gameboard = {
-  currentBoard: (() => {
+const Gameboard =  (() => {
+  const currentBoard = (() => {
     const newBoard = []
     for (let i = 0; i < 9; i++) {newBoard.push(null)}
     return newBoard
-  })(),
-  getBoard: () => Gameboard.currentBoard
-}
+  })();
+  function getBoard() {return currentBoard}
+  function addMove(spot, player) {currentBoard[spot] = player.index}
+  return {
+    getBoard,
+    addMove
+  }
+})();
 
-const winConditions = (() => {
-  const combinations = [ 
+const GameDirector = (() => {
+  const winConditions = [ 
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
@@ -17,34 +22,34 @@ const winConditions = (() => {
     [2, 4, 6],
     [3, 4, 5],
     [6, 7, 8]
-  ]
-  return combinations
-});
+  ];
+  function makeMove(spot, player){
+    Gameboard.addMove(spot,player)
+    player.addPick(spot)
+    return Gameboard.getBoard()
+  };
+  function checkWin(player){
+    if (player.currentPicks === winConditions) {
+      return null
+    }
+  }
+  return {
+    makeMove,
+    checkWin
+  }
+})();
 
-const PlayerOne = {
-  currentPicks: (() => {
-    const newBoard = []
-    return newBoard
-  })(),
-  getPicks: () => PlayerOne.currentPicks
-};
+function playerFactory(index){
+  return {
+    index: index,
+    currentPicks: (() => {
+      const newBoard = []
+      return newBoard
+    })(),
+    getPicks() { return this.currentPicks },
+    addPick(spot) { this.currentPicks.push(spot) }
+  }
+}
 
-
-const PlayerTwo = {
-  currentPicks: (() => {
-    const newBoard = []
-    return newBoard
-  })(),
-  getPicks: () => PlayerTwo.currentPicks
-};
-
-const makeMove = ((spot, player) => {
-  Gameboard.currentBoard[spot] = player
-  if (player === 0) {PlayerOne.currentPicks.push(spot)}
-  else if (player === 1) {PlayerTwo.currentPicks.push(spot)}
-  return Gameboard.getBoard()
-});
-
-const gameDirector = (() => {
-  return null
-});
+let PlayerOne = playerFactory(0);
+let PlayerTwo = playerFactory(1);
